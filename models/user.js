0bbc07ef88a9f0a09399c0d386b531d6f66d1be5
rat619 <40759788+rat619@users.noreply.github.com>
2018-07-23 15:00:07 +0200

    initial office

--- models/user.js
+++ models/user.js
@@ -1,12 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
+  var User = sequelize.define('User', {
     email: DataTypes.STRING,
     username: DataTypes.STRING,
     password: DataTypes.STRING,
     bio: DataTypes.STRING,
-    isAdmin: DataTypes.BOOLEAN,
-                     
+    isAdmin: DataTypes.BOOLEAN
   }, {});
   User.associate = function(models) {
     // associations can be defined here
